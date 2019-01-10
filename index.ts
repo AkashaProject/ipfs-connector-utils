6da2305033e1fb6159917644a3427f4b0c7694ed
Marius Darila <marius.darila@gmail.com>
2019-01-10 23:07:35 +0100

    feat(linked-data): add dag support
    BREAKING CHANGE: incompatible with older versions

--- index.ts
+++ index.ts
@@ -1,115 +1,22 @@
 /// <reference path="typings/main.d.ts"/>
-import * as Promise from 'bluebird';
-import { multihash } from 'is-ipfs';
-import { Readable } from 'stream';
-import { DAGLink, DAGNode } from 'ipld-dag-pb';
+import * as isIpfs from 'is-ipfs';
 import { Buffer } from 'safe-buffer';
 
-
 export default class IpfsApiHelper {
-  public apiClient: any;
-  private _objectMaxSize = 256 * 1024;
-  private _requestTimeout = 60 * 1000;
-  private _encoding = 'base58';
-  private _dagNode: any;
-  private _dagLink: any;
+  public ipfsApi: any;
+
+  // default options for storing DAG nodes
+  public _dagOptions = {
+    format: 'dag-cbor',
+    hashAlg: 'sha2-256',
+  };
 
   /**
-   * Requires an instance of ipfs-api or ipfs
+   * Requires an instance of ipfs-http-client or js-ipfs
    * @param provider
    */
   constructor(provider: any) {
-    this.apiClient = provider;
-    this.apiClient.object = Promise.promisifyAll(this.apiClient.object);
-    this.apiClient.object.patch = Promise.promisifyAll(this.apiClient.object.patch);
-    this.apiClient.block = Promise.promisifyAll(this.apiClient.block);
-    this.apiClient.config = Promise.promisifyAll(this.apiClient.config);
-    this.apiClient = Promise.promisifyAll(this.apiClient);
-    this._dagNode = Promise.promisifyAll(DAGNode);
-    this._dagLink = Promise.promisifyAll(DAGLink);
-
-    if (!this.apiClient.hasOwnProperty('add') && this.apiClient.hasOwnProperty('files')) {
-      Object.defineProperties(
-        this.apiClient,
-        {
-          addAsync: {
-            enumerable: true,
-            value: Promise.promisify(this.apiClient.files.add),
-          },
-          catAsync: {
-            enumerable: true,
-            value: Promise.promisify(this.apiClient.files.cat),
-          },
-        },
-      );
-    }
-  }
-
-  /**
-   *
-   * @returns {string}
-   */
-  public get ENCODING() {
-    return this._encoding;
-  }
-
-  /**
-   *
-   * @returns {number}
-   */
-  public get OBJECT_MAX_SIZE() {
-    return this._objectMaxSize;
-  }
-
-  /**
-   *
-   * @returns {Object}
-   * @constructor
-   */
-  public get DAGNode() {
-    return this._dagNode;
-  }
-
-  /**
-   *
-   * @returns {Object}
-   * @constructor
-   */
-  public get DAGLink() {
-    return this._dagLink;
-  }
-
-  /**
-   *
-   * @returns {number}
-   */
-  public get REQUEST_TIMEOUT() {
-    return this._requestTimeout;
-  }
-
-  /**
-   * Set the maximum size for an ipfs object
-   * Objects bigger than this value will be split
-   * @param size
-   */
-  public setObjectMaxSize(size: number) {
-    this._objectMaxSize = size;
-  }
-
-  /**
-   * Set ipfs-api call timeout value
-   * @param time
-   */
-  public setRequestTimeout(time: number) {
-    this._requestTimeout = time;
-  }
-
-  /**
-   * Set ipfs hash encoding
-   * @param encode
-   */
-  public setEncoding(encode: string) {
-    this._encoding = encode;
+    this.ipfsApi = provider;
   }
 
   /**
@@ -117,288 +24,58 @@ export default class IpfsApiHelper {
    * @param data
    * @returns {Buffer}
    */
-  public static toDataBuffer(data: Object) {
-    if (Buffer.isBuffer(data)) {
+  public static toDataBuffer(data: object) {
+    if (IpfsApiHelper.Buffer.isBuffer(data)) {
       return data;
     }
-    return Buffer.from(JSON.stringify(data));
+    return IpfsApiHelper.Buffer.from(JSON.stringify(data));
+  }
+
+  public static get isIpfs() {
+    return isIpfs;
+  }
+
+  public static get Buffer() {
+    return Buffer;
   }
 
   /**
-   * Extract data from ipfs DAG instance
-   * @param rawData
-   * @returns {object}
-   */
-  public static fromRawData(rawData: any) {
-    const jsonData = IpfsApiHelper.fromRawObject(rawData);
-    let returned: any;
-    try {
-      returned = JSON.parse(jsonData.data);
-    } catch (err) {
-      returned = jsonData.data;
-    }
-    return returned;
-  }
-
-  /**
-   *
-   * @param rawObject
-   * @returns {string|any|{type: "Buffer", data: any[]}|Object}
-   */
-  public static fromRawObject(rawObject: any) {
-    return rawObject.toJSON();
-  }
-
-  /**
-   * Add data to ipfs
+   * Create a node for the `data`
    * @param data
-   * @param isFile
-   * @param checkIfHash
-   * @returns {any}
    */
-  public add(data: any, isFile = false, checkIfHash = false): Promise<{ hash: string, size: number }> {
-    let dataBuffer: Buffer;
-    if (checkIfHash && multihash(data)) {
-      return this
-        .apiClient
-        .block
-        .statAsync(data)
-        .then((result: any) => {
-          return { hash: result.key, size: result.size };
-        });
-    }
-    if (Buffer.isBuffer(data) || isFile) {
-      dataBuffer = data;
-    } else {
-      dataBuffer = IpfsApiHelper.toDataBuffer(data);
-    }
-    // use file api for files and big js objects
-    if (dataBuffer.length > this.OBJECT_MAX_SIZE || isFile) {
-      return this.addFile(dataBuffer);
-    }
-    return this.addObject(dataBuffer);
+  public async add(data: object) {
+    return await this.ipfsApi.dag.put(data, this._dagOptions);
   }
 
   /**
-   * Save Buffer instance to an ipfs object
-   * @param data
-   * @returns {Bluebird}
-   */
-  public addObject(data: Buffer) {
-    return this.createNode(data, []);
-  }
-
-  /**
-   * Save Buffer instance using ipfs file api
-   * @param dataBuffer
-   * @returns {Bluebird<{hash, size}>}
-   */
-  public addFile(dataBuffer: Buffer) {
-    return this.apiClient
-      .addAsync(dataBuffer)
-      .then((file: any[]) => {
-        return { hash: file[0].hash, size: file[0].size };
-      });
-  }
-
-  /**
-   *
-   * @param objectHash
-   * @param isFile
-   * @returns {Promise<Buffer> | Promise<Object>}
-   */
-  public get(objectHash: string, isFile = false) {
-    if (isFile) {
-      return this.getFile(objectHash);
-    }
-    return this.getObject(objectHash);
-  }
-
-  /**
-   * Get data from an ipfs hash
-   * Returns a js or DAG object(raw)
-   * @param objectHash
-   * @param raw
-   * @returns {Bluebird<U>}
-   */
-  public getObject(objectHash: string, raw?: boolean) {
-    return this.apiClient
-      .object
-      .getAsync(objectHash, { enc: this._encoding })
-      .timeout(this.REQUEST_TIMEOUT)
-      .then((rawData: any) => {
-        if (raw) {
-          return rawData;
-        }
-        return IpfsApiHelper.fromRawData(rawData);
-      });
-  }
-
-  /**
-   * Get contents of a file from ipfs
-   * @param hash
-   * @returns {Bluebird<Buffer>}
-   */
-  public getFile(hash: string) {
-    return new Promise((resolve, reject) => {
-      const chunks: Buffer[] = [];
-      let fileLength = 0;
-      return this.apiClient
-        .catAsync(hash)
-        .timeout(this.REQUEST_TIMEOUT)
-        .then((stream: Readable) => {
-          if (stream.readable) {
-            stream
-              .on('error', (err: Error) => {
-                return reject(err);
-              })
-              .on('data', (data: Buffer) => {
-                fileLength += data.length;
-                chunks.push(data);
-              })
-              .on('end', () => {
-                const file = Buffer.concat(chunks, fileLength);
-                resolve(file);
-              });
-            return;
-          }
-          return resolve(stream);
-        });
-    });
-  }
-
-  /**
-   * Get statistics(size, links, etc) for an ipfs hash
-   * @param objectHash
-   * @returns {Bluebird<Object>}
-   */
-  public getStats(objectHash: string) {
-    return this.apiClient
-      .object
-      .statAsync(objectHash, { enc: this._encoding })
-      .timeout(this.REQUEST_TIMEOUT)
-      .then((result: Object) => {
-        return result;
-      });
-  }
-
-  /**
-   * Get ipfs hash DAGNode links
-   * @param hash
-   * @param enc
-   * @returns {Bluebird}
-   */
-  public getLinks(hash: string, enc = this._encoding) {
-    return this.apiClient.object.linksAsync(hash, { enc: enc });
-  }
-
-  /**
-   * Patch an ipfs object
-   * @param hash
-   * @param newData
-   * @param enc
-   * @returns {PromiseLike<T>}
-   */
-  public updateObject(hash: string, newData: Object, enc = this._encoding) {
-    return this.get(hash)
-      .then((dataResponse: Object) => {
-        const updatedObject = Object.assign({}, dataResponse, newData);
-        const dataBuffer = IpfsApiHelper.toDataBuffer(updatedObject);
-        // this returns a DAGNode
-        return this.apiClient
-          .object
-          .patch
-          .setDataAsync(hash, dataBuffer, { enc: 'base58' });
-      })
-      .then((dagNode: any) => {
-        return IpfsApiHelper.fromRawObject(dagNode);
-      });
-  }
-
-  /**
-   * Create an ipfs object containg data and links
-   * @param data
-   * @param links
-   * @returns {Bluebird<U2|U1>|Thenable<U>|Bluebird<U>|Promise<TResult>|Bluebird<R>|Promise<T>|any}
-   */
-  public createNode(data: any, links: any[]) {
-    return this._dagNode
-      .createAsync(IpfsApiHelper.toDataBuffer(data), links)
-      .then((dagNode: any) => {
-        return this.apiClient
-          .object
-          .putAsync(dagNode);
-      })
-      .then((node: any) => {
-        const format = node.toJSON();
-        return { hash: format.multihash, size: format.size };
-      });
-  }
-
-  /**
-   *
-   * @param hash
-   * @param names
-   * @returns {Bluebird<undefined|T|number|any>}
-   */
-  public findLinks(hash: string, names: string []) {
-    return this.getObject(hash, true)
-      .then((dagNode: any) => {
-        const format = dagNode.toJSON();
-        return format.links.filter((link: any) => names.indexOf(link.name) !== -1);
-      });
-  }
-
-  /**
-   *
-   * @param start
+   * Read data from IPFS path
+   * @param cid
    * @param path
-   * @returns {Function}
    */
-  public findLinkPath(start: string, path: string []) {
-    const _this = this;
-    return Promise.coroutine(function* () {
-      if (!multihash(start) || !path.length) {
-        throw new Error('Invalid path');
-      }
-      let index = 0;
-      let currentPath = yield _this.findLinks(start, path.slice(index, ++index));
-      while (index < path.length && currentPath.length) {
-        currentPath = yield _this.findLinks(currentPath[0].multihash, path.slice(index, ++index));
-      }
-      return currentPath;
-    })();
+  public async get(cid: string | object, path: string) {
+    return await this.ipfsApi.dag.get(cid, path);
   }
 
-
   /**
-   * Creates an ipfs object containing a link
-   * Handles ipfs hash for data
+   * Creates a node from `data` and references it in a link
    * @param data
    * @param name
-   * @param root
-   * @param enc
-   * @returns {PromiseLike<{size: number, hash: string}>|Promise<TResult|{size: number, hash: string}>|Bluebird<U>|PromiseLike<TResult|{size: number, hash: string}>|Thenable<U>|PromiseLike<TResult2|TResult1>|any}
    */
-  public addLinkFrom(data: any, name: string, root: string, enc ?: string) {
-    return this.add(data)
-      .then((result: { size: number, hash: string }) => {
-        return this.addLink({ name, size: result.size, hash: result.hash }, root, enc);
-      });
+  public async addLinkFrom(data: object, name: string) {
+    const cid = await this.add(data);
+    return await this.addLink(cid, name);
   }
 
   /**
-   * Patches an existing hash with a new link
-   * @param link
-   * @param root
-   * @param enc
-   * @returns {PromiseLike<T>|Promise<TResult|T>|Bluebird<U>|PromiseLike<TResult|T>|Thenable<U>|PromiseLike<TResult2|TResult1>|any}
+   * Creates a node with a property linked to a CID
+   * @param cid
+   * @param name
    */
-  public addLink(link: { name: string, size: number, hash: string }, root: string, enc = this._encoding) {
-    const objLink = new DAGLink(link.name, link.size, link.hash);
-    return this.apiClient.object.patch.addLinkAsync(root, objLink, { enc: enc })
-      .then((dagNode: any) => {
-        return IpfsApiHelper.fromRawObject(dagNode);
-      });
+  public async addLink(cid: string | object, name: string) {
+    if (!IpfsApiHelper.isIpfs.cid(cid)) {
+      throw new Error('Supplied CID is not valid');
+    }
+    const linkedCid = (typeof cid === 'string' || cid instanceof String) ? new this.ipfsApi.types.CID(cid) : cid;
+    return await this.add({ [name]: linkedCid });
   }
 }
---
+++ test.ts
@@ -0,0 +1,296 @@
+import IpfsApiHelper from './index';
+import * as IPFS from 'ipfs';
+import { expect } from 'chai';
+import * as rimraf from 'rimraf';
+
+describe('ipfs-connector-utils', function () {
+  let helperInstance: IpfsApiHelper;
+  let provider: any;
+
+  const longText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been` +
+    `the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and` +
+    `scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into` +
+    `electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of` +
+    `Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like` +
+    `Aldus PageMaker including versions of Lorem Ipsum.`;
+  this.timeout(100000);
+
+  before(function (done) {
+    provider = new IPFS({ repo: 'test-repo', start: true });
+    provider.on('start', () => {
+      helperInstance = new IpfsApiHelper(provider);
+    });
+    setTimeout(() => {
+      expect(helperInstance).to.exist;
+      done();
+    }, 9500);
+  });
+
+  it('adds an object to ipfs', function () {
+    return helperInstance.add({ data: '{}' })
+      .then((cid) => {
+        expect(cid).to.exist;
+        expect(cid).to.have.property('multihash');
+        expect(cid).to.have.property('codec');
+        expect(cid).to.have.property('version');
+      });
+  });
+
+  it('transforms object to buffer', function () {
+    const x = { a: 1 };
+    const expected = Buffer.from(JSON.stringify(x));
+    const actual = IpfsApiHelper.toDataBuffer(x);
+    expect(actual.toString()).to.equal(expected.toString());
+  });
+
+  it('preserves buffer', function () {
+    const initial = Buffer.from(JSON.stringify({ q: 1 }));
+    const actual = IpfsApiHelper.toDataBuffer(initial);
+    expect(actual.toString()).to.equal(initial.toString());
+  });
+
+  it('gets data from node', function() {
+    return helperInstance.add({q: {v: 1}})
+      .then((cid1) => helperInstance.get(cid1, '/q/v'))
+      .then(result => {
+        expect(result).to.exist;
+        expect(result).to.have.property('value');
+        expect(result.value).to.equal(1);
+      });
+  });
+
+  it('creates a cid with a linked property from data', function() {
+    return helperInstance.addLinkFrom({firstName: 'John', lastName: 'Doe'}, 'name')
+      .then(cid => {
+        expect(cid).to.exist;
+        expect(cid).to.have.property('multihash');
+        expect(cid).to.have.property('codec');
+        expect(cid).to.have.property('version');
+        return helperInstance.get(cid, '/name/firstName');
+      })
+      .then(result => {
+        expect(result.value).to.equal('John');
+      })
+      .catch(err => expect(err).to.not.exist);
+  });
+
+  it('creates a cid link from encoded string', function() {
+    return helperInstance.addLink('zdpuB2CAUgUbyJ4s8SmqcGGaGfs7bG337dDNty8NSCR3fhgRw', 'details')
+      .then(cid => {
+        expect(cid).to.exist;
+        expect(cid).to.have.property('multihash');
+        expect(cid).to.have.property('codec');
+        expect(cid).to.have.property('version');
+        return helperInstance.get(cid, '/details/lastName');
+      })
+      .then(result => {
+        expect(result.value).to.equal('Doe');
+      })
+      .catch(err => expect(err).to.not.exist);
+  });
+
+  it('fails to create a cid link from bad encoded string', function() {
+    return helperInstance.addLink('zdpuB2CAUgUbyJ4s8SmqqcGGaGfs7bG337dDNty8NSCR3fhgRw', 'details')
+      .then(cid => {
+        expect(cid).to.not.exist;
+      })
+      .catch(err => { expect(err).to.exist; });
+  });
+  /*
+
+
+
+      it('adds buffer instance to ipfs', function () {
+          const actual = Buffer.from(JSON.stringify({ a: 1, b: 2 }));
+          return helperInstance.add(actual).then(node => {
+              expect(node).to.have.property('hash');
+          });
+      });
+
+      it('adds plain text to ipfs', function () {
+          return helperInstance.add(Buffer.from('testing test'))
+              .then((node) => {
+                  expect(node).to.have.property('hash');
+                  return helperInstance.get(node.hash)
+                      .then((data: Buffer) => {
+                          expect(data).to.exist;
+                      })
+              })
+      });
+
+      it('adds raw buffer using api.addFile', function () {
+          const buf = Buffer.from(JSON.stringify({ a: 1, b: 2, c: 3 }));
+          return helperInstance.addFile(buf).then((node: any) => {
+              expect(node).to.have.property('hash');
+          });
+      });
+
+      it('updates from existing object', function () {
+          const initialObj = { a: 1, b: 2 };
+          return helperInstance.add(initialObj)
+              .then((node) => {
+                  const patchAttr = { b: 3 };
+                  ipfsHash1 = node.hash;
+                  expect(node).to.exist;
+                  return helperInstance.updateObject(node.hash, patchAttr).then((result: any) => {
+                      result.data = JSON.parse(result.data);
+                      expect(result.data.a).to.equal(initialObj.a);
+                      expect(result.data.b).to.equal(patchAttr.b);
+                      expect(result.multihash).to.exist;
+                  });
+              });
+      });
+
+      it('constructs object link from hash', function () {
+          return helperInstance.add({ a: 1, b: 2 })
+              .then((node) => {
+                  expect(node).to.exist;
+                  return helperInstance
+                      .addLinkFrom({ coco: 1 }, 'testLink', node.hash)
+                      .then((result) => {
+                          expect(result.links.length).to.be.above(0);
+                      });
+              });
+      });
+
+      it('creates node with links', function () {
+          const links = [{ name: 'testFile', size: 12, multihash: 'QmPMH5GFmLP2oU8dK7i4iWJyX6FpgeK3gT6ZC6xLLZQ9cW' },
+              { name: 'testLink', size: 12, multihash: ipfsHash1 }];
+          return helperInstance
+              .createNode({ test: 2 }, links)
+              .then((result: any) => {
+                  ipfsHash = result.hash;
+                  expect(result).to.have.property('hash');
+              })
+      });
+
+      it('gets object links', function () {
+          return helperInstance.getLinks(ipfsHash)
+              .then((result: any) => {
+                  expect(result.length).to.equal(2);
+                  rawObject = result;
+              });
+      });
+
+      it('transforms raw object', function () {
+          return helperInstance.DAGNode.createAsync(Buffer.from(JSON.stringify({ test: 'test' })))
+              .then((rawObject: any) => {
+                  const result = IpfsApiHelper.fromRawObject(rawObject);
+                  expect(result).to.have.property('data');
+              })
+      });
+
+      it('gets data from raw object', function () {
+          return helperInstance.DAGNode.createAsync(Buffer.from(JSON.stringify({ test: 'test1' })))
+              .then((rawObject: any) => {
+                  const result = IpfsApiHelper.fromRawData(rawObject);
+                  expect(result).to.have.property('test');
+              })
+      });
+
+      it('resolves a given link path', function () {
+          return helperInstance
+              .addLinkFrom({ test: true }, 'firstLink', ipfsHash)
+              .then((result) => {
+                  expect(result).to.exist;
+                  return helperInstance.addLink(
+                      { name: 'lastLink', hash: result.multihash, size: result.size },
+                      ipfsHash
+                  ).then((patched: any) => {
+                      return helperInstance.findLinkPath(patched.multihash, ['lastLink', 'firstLink'])
+                          .then((final: any) => {
+                              expect(final).to.exist;
+                              return helperInstance.get(final[0].multihash)
+                                  .then((finalData: any) => {
+                                      expect(finalData).to.have.property('test');
+                                  })
+                          });
+                  });
+              });
+      });
+
+      it('fails to find link path', function () {
+          return helperInstance.findLinkPath(ipfsHash, [])
+              .then((data: any) => {
+                  expect(data).to.not.exist;
+              })
+              .catch((err: any) => {
+                  expect(err).to.exist;
+              })
+      });
+
+      it('should force to file api add', function () {
+          return helperInstance.addFile(Buffer.from('TEST'))
+              .then((node: any) => {
+                  return helperInstance.getFile(node.hash)
+                      .then((result) => {
+                          expect(result.toString()).to.eql('TEST');
+                      });
+              });
+      });
+
+      it('should have a default encoding', function () {
+          expect(helperInstance.ENCODING).to.exist;
+      });
+
+      it('should have access to DAGNode and DAGLink', function () {
+          expect(helperInstance.DAGNode).to.have.property('createAsync');
+          expect(helperInstance.DAGLink).to.have.property('createAsync');
+      });
+
+      it('sets max ipfs object size', function () {
+          helperInstance.setObjectMaxSize(100);
+          expect(helperInstance.OBJECT_MAX_SIZE).to.equal(100);
+      });
+
+      it('sets response timeout', function () {
+          helperInstance.setRequestTimeout(3000);
+          expect(helperInstance.REQUEST_TIMEOUT).to.equal(3000);
+      });
+
+      it('adds object using file api', function () {
+          return helperInstance.add({ q: longText })
+              .then((node) => {
+                  expect(node).to.have.property('hash');
+                  return helperInstance.get(node.hash, true)
+                      .then((result: Buffer) => {
+                          expect(JSON.parse(result.toString())).deep.equal({ q: longText });
+                      });
+              });
+      });
+
+      it.skip('gets hash stats', function () {
+          return helperInstance
+              .getStats(statHash)
+              .then((stats: any) => {
+                  console.log(stats);
+                  expect(stats).to.have.property('DataSize');
+              });
+      });
+
+      it('returns same hash on add if checkIfHash is present', function () {
+          return helperInstance
+              .add(statHash, false, true)
+              .then((result: any) => {
+                  console.log(statHash, result);
+                  expect(result).to.have.property('hash');
+                  expect(result).to.have.property('size');
+              });
+      });
+
+      it('sets encoding for ipfs hash', function (done) {
+          helperInstance.setEncoding('base64');
+          expect(helperInstance.ENCODING).to.equal('base64');
+          provider.stop(() => done());
+      });*/
+
+  after(function (done) {
+    rimraf('./test-repo', (err) => {
+      if (err) {
+        console.log(err);
+      }
+      done();
+      process.exit(); // weird hang
+    });
+  });
+});
